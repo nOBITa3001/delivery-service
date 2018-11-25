ef5c857f3d0d06aaa8e1f4c0c9d76f23a2f8c9e3
Pongsakorn Thanopassakul <pongsakorn.th@dreamlines.com>
2018-11-25 12:26:28 +0100

    + add rules for GetDeliveryCostHandlerRequest

---
+++ src/DS/DS.Handlers/Validators/GetDeliveryCostHandlerRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using DS.Handlers.Requests;
+using FluentValidation;
+
+namespace HBFA.Handlers.Validators
+{
+    public class GetDeliveryCostHandlerRequestValidator : AbstractValidator<GetDeliveryCostHandlerRequest>
+    {
+        public GetDeliveryCostHandlerRequestValidator()
+        {
+            RuleFor(request => request.Route).NotEmpty();
+        }
+    }
+}
