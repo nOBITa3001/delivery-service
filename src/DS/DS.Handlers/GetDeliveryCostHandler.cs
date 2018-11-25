d5e55094e42f77eef1e3c2970a8a936a56ccdbf0
Pongsakorn Thanopassakul <pongsakorn.th@dreamlines.com>
2018-11-25 12:22:57 +0100

    + initialize GetDeliveryCostHandler

---
+++ src/DS/DS.Handlers/GetDeliveryCostHandler.cs
@@ -0,0 +1,28 @@
+﻿using DS.Contracts.OperationResponse;
+using DS.Handlers.Abstract;
+using DS.Handlers.Requests;
+using DS.Handlers.Responses;
+using DS.Handlers.Strategies.Factories;
+using FluentValidation;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace DS.Handlers
+{
+    public class GetDeliveryCostHandler : HandlerBase<GetDeliveryCostHandlerRequest, GetDeliveryCostHandlerResponse>
+    {
+        public GetDeliveryCostHandler(
+            ILogger<GetDeliveryCostHandler> logger,
+            IValidator<GetDeliveryCostHandlerRequest> validator,
+            IHandlerExceptionStrategyFactory handlerExceptionStrategyFactory)
+            : base(logger, validator, handlerExceptionStrategyFactory)
+        {
+        }
+
+        protected override async Task<IOperationResponse<GetDeliveryCostHandlerResponse>> HandleAsync(GetDeliveryCostHandlerRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
