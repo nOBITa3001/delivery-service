625d34688457f00d2c9c91ae52d44d8ae1a87b93
Pongsakorn Thanopassakul <pongsakorn.th@dreamlines.com>
2018-11-25 19:23:41 +0100

    + comment the rest exception strategy test

---
+++ src/DS/DS.Handlers.UnitTests/Strategies/Exceptions/HandlerDomainModelExceptionStrategyTests.cs
@@ -0,0 +1,7 @@
+﻿namespace DS.Handlers.UnitTests.Strategies.Exceptions
+{
+    public class HandlerDomainModelExceptionStrategyTests
+    {
+        // Please see test concept at HandlerDataAccessNotFoundExceptionStrategyTests class
+    }
+}
