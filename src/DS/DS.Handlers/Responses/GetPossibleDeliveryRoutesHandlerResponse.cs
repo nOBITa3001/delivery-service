b2dea7cdb339ed7773a26717c3439928130b6a77
Pongsakorn Thanopassakul <pongsakorn.th@dreamlines.com>
2018-11-25 21:25:24 +0100

    + change PossibleRoute from nullable int to int

--- src/DS/DS.Handlers/Responses/GetPossibleDeliveryRoutesHandlerResponse.cs
+++ src/DS/DS.Handlers/Responses/GetPossibleDeliveryRoutesHandlerResponse.cs
@@ -4,9 +4,9 @@ namespace DS.Handlers.Responses
 {
     public class GetPossibleDeliveryRoutesHandlerResponse : HandlerResponseBase
     {
-        public int? PossibleRoute { get; }
+        public int PossibleRoute { get; }
 
-        public GetPossibleDeliveryRoutesHandlerResponse(int? possibleRoute)
+        public GetPossibleDeliveryRoutesHandlerResponse(int possibleRoute)
         {
             PossibleRoute = possibleRoute;
         }
